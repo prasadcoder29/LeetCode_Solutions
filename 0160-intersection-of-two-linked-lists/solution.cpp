63e85a64a11da5110a54f80d5d53d413222aac16
prasadcoder29 <prasadcoder29@gmail.com>
2026-09-07 13:42:39 +0000

    [LeetCode Sync] Runtime - 59 ms (40.22%), Memory - 24 MB (25.06%)

---
+++ 0160-intersection-of-two-linked-lists/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        int len1=0,len2=0;
+        while(temp1!=NULL){
+            len1++;
+            temp1=temp1->next;
+        }
+        while(temp2!=NULL){
+            len2++;
+            temp2=temp2->next;
+        }
+        temp1=headA;
+        temp2=headB;
+        while(len1>len2){
+            temp1=temp1->next;
+            len1--;
+        }
+        while(len1<len2){
+            temp2=temp2->next;
+            len2--;
+        }
+        while(temp1!=temp2){
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return temp1;
+    }
+};
